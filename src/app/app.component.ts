5ec1b32dc0f54fc11de7e85fa3158f53e9178e17
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-06-20 16:11:12 -0500

    Se retiraron componentes complejos y solo se dejó uno simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import ButtonComponent from 'src/stories/button.component';
+import ButtonComponent from 'src/stories/button/button.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import ButtonComponent from 'src/stories/button.component';
+import ButtonComponent from 'src/stories/Button/button.component';
 
 @NgModule({
   declarations: [
